288506f18d99c6f75fe6344c5d0d614815081579
Richard Li <richzli01@gmail.com>
2020-06-12 11:48:38 -0500

    did jinja stuff, started navbar, added some images

--- app/routes.py
+++ app/routes.py
@@ -7,21 +7,21 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title = "about", nav = True)
 
 @app.route("/projects")
 def projects():
-    return render_template("projects.html")
+    return render_template("projects.html", title = "projects", nav = True)
 
 @app.route("/blog")
 def blog():
-    return render_template("blog.html")
+    return render_template("blog.html", title = "blog", nav = True)
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", title = "contact", nav = True)
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html")
+    return render_template("404.html", title = "404")
