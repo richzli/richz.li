ee7838ccc5de0d76562b88deba848f24b14504b8
Richard Li <richzli01@gmail.com>
2020-06-13 17:21:30 -0500

    more navbar stuff

--- app/routes.py
+++ app/routes.py
@@ -1,25 +1,27 @@
 from flask import render_template
 from app import app
 
+pages = ("about", "projects", "blog", "contact")
+
 @app.route("/")
 def home():
     return render_template("home.html")
 
 @app.route("/about")
 def about():
-    return render_template("about.html", title = "about", nav = True)
+    return render_template("about.html", title = "about", nav = pages)
 
 @app.route("/projects")
 def projects():
-    return render_template("projects.html", title = "projects", nav = True)
+    return render_template("projects.html", title = "projects", nav = pages)
 
 @app.route("/blog")
 def blog():
-    return render_template("blog.html", title = "blog", nav = True)
+    return render_template("blog.html", title = "blog", nav = pages)
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html", title = "contact", nav = True)
+    return render_template("contact.html", title = "contact", nav = pages)
 
 
 @app.errorhandler(404)
