0095b540a3682a51bf87e3740b6f97e261a9a887
Richard Li <richzli01@gmail.com>
2020-06-14 13:10:42 -0500

    mostly functional navbar, working on full-page fade

--- app/static/js/nav.js
+++ app/static/js/nav.js
@@ -1,12 +1,14 @@
 $(window).bind("pageshow", () => { $("main").fadeIn(500); });
 
 $(function() {
-    $(".ml-away").click(function(e) {
+    $(".nav-item a").click(function(e) {
         var evt = window.event || e;
         evt.preventDefault();
+        $(".nav-curr").animate({bottom: "-=0.3rem"}, {duration: 500, queue: false});
+        $(evt.target).parent().toggleClass("hovered");
         $("main").fadeOut(500);
         setTimeout(function() {
             window.location.href = $(evt.target).attr("href");
-        }, 1000);
+        }, 500);
     });
 });
