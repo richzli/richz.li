d541727460b18a1e28efeaf7462855409110a80c
Richard Li <richzli01@gmail.com>
2020-06-23 23:29:10 -0500

    changed up navbar styling a bit

--- app/static/js/nav.js
+++ app/static/js/nav.js
@@ -4,7 +4,7 @@ $(function() {
     $(".nav-item a").click(function(e) {
         var evt = window.event || e;
         evt.preventDefault();
-        $(".nav-curr").animate({bottom: "-=0.3rem"}, {duration: 500, queue: false});
+        $(".nav-curr").animate({bottom: "-=0.2rem"}, {duration: 500, queue: false});
         $(evt.target).parent().toggleClass("hovered");
         $("main").fadeOut(500);
         setTimeout(function() {
